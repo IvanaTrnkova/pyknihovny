5b93111d3e6f8d279310ca69e7d521a492d9ae60
Ivana Trnkova <xtrnkova@mendelu.cz>
2017-03-21 19:22:56 +0100

    pridani sablony a html

--- project/hello.py
+++ project/hello.py
@@ -1,20 +1,24 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 #__name__ promenna nastavena pythonem na jmeno aktualniho modulu
+# v prvni prvne return string, az pak url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
 #routa - funkce přiřazená k webové adrese, říká, co se stane když se na ni ukáže
 def hello():
-    return url_for()
+	return url_for("hello_english",
+					username="Ivana",
+					count=3)
 
 
 @app.route('/hello/')
 @app.route('/hello/<username>/')
 @app.route('/hello/<username>/<int:count>/')
 def hello_english(username=None, count=1):
-    return 'Hello {}! '.format(username) * count
+	return render_template("hello.html", name=username)
 
 
 
